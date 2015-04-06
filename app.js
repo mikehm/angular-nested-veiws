be746d6aa73b63c8a2bba02d1c6546ac972573bd
Mike <mike@veryona.com>
2015-04-06 14:39:32 -0400

    Modified app.js and partial-home-list.html. Created nested states and linked the to the home state.

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ app.config(function($stateProvider, $urlRouterProvider){
 	$urlRouterProvider.otherwise('/home');
 
 	$stateProvider
-			.state('home', {
+		.state('home', {
 			url: '/home',
 			templateUrl: 'partial-home.html'
 		})
-		.state('about', {
 
-		})
+
+	.state('home.list', {
+        url: '/list',
+        templateUrl: 'partial-home-list.html',
+        controller: function($scope) {
+           $scope.cars = ['Honda', 'Toyota'];
+        }
+    })
+
+		.state('home.paragraph', {
+        url: '/paragraph',
+        template: 'I could sure use a drink right now.'
+    })
+		
 		
 })
