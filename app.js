c1d6cf9940adb0e94ebafa5a31504792f3cf7527
Mike <mike@veryona.com>
2015-04-06 15:13:53 -0400

    Modified multiple views

--- app.js
+++ app.js
@@ -7,19 +7,67 @@ app.config(function($stateProvider, $urlRouterProvider){
 		.state('home', {
 			url: '/home',
 			templateUrl: 'partial-home.html'
-		})
-
-
+	})
 	.state('home.list', {
         url: '/list',
         templateUrl: 'partial-home-list.html',
         controller: function($scope) {
            $scope.cars = ['Honda', 'Toyota'];
-        }
-    })
+        }  	
+    })	
 
-		.state('home.paragraph', {
+	.state('home.paragraph', {
         url: '/paragraph',
         template: 'I like Japanese cars'
-		})	
 	})
+	.state('about', {
+		url:'/about',
+		views: {
+			'':{templateUrl:'partial-about.html'},
+			'columnOne@about':{template: 'Look I am a column'},
+			'columnTwo@about':{
+				templateUrl: 'table-data.html',
+				controller:'MyCtrl' 
+			}
+	}
+})
+
+});
+
+app.controller('MyCtrl', function($scope){	
+	$scope.message = 'test';	
+	$scope.autos = [
+		{
+			name:'Nissan',
+			price: 17000
+		},
+
+		{
+			name: 'Lexus',
+			price: 50000
+		},
+		{
+			name: 'Ferrari',
+			price: 150000
+		}
+	];
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
